955b99c1b2c03cfa07b7a54442b2329e7cb19728
Jeremy Arimado <jarimado@gmail.com>
2016-11-05 22:53:39 +1100

    added loop and attak sounds

--- game/index.js
+++ game/index.js
@@ -18,7 +18,7 @@ var Game = function() {
 			let rotation = this.rotation(position);
 			let choice = questions[this.step].choices[i];
 
-	    	let entity = yo`<a-entity cursor-listener obj-model="obj: #fly-b-obj; mtl: #fly-b-mtl" data-choice="${choice}" position="${position.x} ${position.y} ${position.z}" scale="1 1 1" rotation="${rotation.x} ${rotation.y} ${rotation.z}">
+	    	let entity = yo`<a-entity cursor-listener obj-model="obj: #fly-b-obj; mtl: #fly-b-mtl" data-choice="${choice}" position="${position.x} ${position.y} ${position.z}" scale="1 1 1" rotation="${rotation.x} ${rotation.y} ${rotation.z}" sound="src: #sound-attack; on: click">
 						<a-animation attribute="scale" dur="200" fill="forwards" to="1 1 1" repeat="0"></a-animation>
 						<a-entity material="color: white" text="text: ${choice}; size: 0.24" position="-0.2 0.1 0"></a-entity>
 				</a-entity>`;
