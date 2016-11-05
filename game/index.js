3bae641cf09e9c408dc71565c508cd645977e46c
katiaeirin <katiaeirin@users.noreply.github.com>
2016-11-05 21:35:38 +1100

    Updates

---
+++ game/index.js
@@ -0,0 +1,66 @@
+const questions = require('./questions');
+const yo = require('yo-yo');
+
+var Game = function() {
+	
+	this.step = 0;
+
+	this.init = () => {
+
+		this.question();
+	}
+
+	this.question = () => {
+
+		for (let i = 0; i < questions[this.step].choices.length; i++) {
+
+			let position = this.position();
+			let rotation = this.rotation(position);
+			let choice =  questions[this.step].choices[i];
+
+	    	let entity = yo`<a-entity cursor-listener obj-model="obj: #fly-b-obj; mtl: #fly-b-mtl" data-choice="${choice}" position="${position.x} ${position.y} ${position.z}" scale="1 1 1" rotation="${rotation.x} ${rotation.y} ${rotation.z}"></a-entity>`;
+
+			scene.appendChild(entity)
+			console.log('Added Question');
+	    }
+
+	}
+
+	this.position = () => {
+
+		let position = {
+			x: this.getRand(-10, 10), 
+			y: this.getRand(1, 3),
+			z: this.getRand(-2, -12),
+		}
+
+		return position;
+	}
+
+	this.rotation = (position) => {
+
+		let rotation = {
+			x: this.getRand(-5, 5), 
+			y: position.x * -8,
+			z: this.getRand(-5, 5), 
+		}
+
+		return rotation;
+	}
+
+	this.getRand = (min, max) => {
+    	return Math.round(Math.random() * (max - min) + min);
+	}
+}
+
+window.onload = function() { 
+
+	var game = new Game();
+	let scene = document.querySelector('#scene');
+
+	if (scene.hasLoaded) {
+		game.init();
+	} else {
+		scene.addEventListener('loaded', game.init);
+	}
+}
--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// require('aframe-animation-component');
-//
-// var el = document.querySelector('#mario');
-//
-// // TweenMax.to(this.state.sphere.position, 1, {
-// //   bezier:[{x:1, y:1}, {x:0, y:2}, {x:-1, y:1}, {x:0, y:0}, {x:1, y:1}],
-// //   ease:Linear.easeNone,
-// //   repeat: -1,
-// //   onUpdate: () => {
-// //     this.setState({sphere: {
-// //       position: this.state.sphere.position
-// //     }});
-// //   }
-// // });
+require('aframe-text-component');
+require('./game/index.js');
+require('./game/events.js');
