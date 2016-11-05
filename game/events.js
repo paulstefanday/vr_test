ebe368a0d7d5d956de2e0fdc3c259f427c2c8e38
Paul <paul@paulday.com.au>
2016-11-05 22:07:02 +1100

    removed mouseover

--- game/events.js
+++ game/events.js
@@ -1,23 +1,29 @@
 const yo = require('yo-yo')
 
-function addAnimation(el) {
-  let child = yo`<a-animation attribute="scale" dur="200" fill="forwards" to="1.4 1.4 1.4" repeat="0"></a-animation>`
-  el.appendChild(child)
-}
-
-function leaveAnimation(el) {
-  el.innerHTML = ''
-  let child = yo`<a-animation attribute="scale" dur="200" fill="forwards" to="1 1 1" repeat="0"></a-animation>`
-  el.appendChild(child)
-}
+// function addAnimation(el) {
+//   // el.setAttribute('')
+//   console.log(el.children[0])
+//   // let choice = el.getAttribute('data-choice')
+//   // let child = yo`<a-animation attribute="scale" dur="200" fill="forwards" to="1.4 1.4 1.4" repeat="0"></a-animation>`
+//   // let text = yo`<a-entity material="color: white" text="text: ${choice}; size: 0.3" position="-0.3 0.1 0"></a-entity>`
+//   // el.appendChild(child)
+//   // el.appendChild(text)
+// }
+//
+// function leaveAnimation(el) {
+//   console.log(el.children[0])
+//   // el.innerHTML = ''
+//   // let choice = el.getAttribute('data-choice')
+//   // let child = yo`<a-animation attribute="scale" dur="200" fill="forwards" to="1 1 1" repeat="0"></a-animation>`
+//   // let text = yo`<a-entity material="color: white" text="text: ${choice}; size: 0.3" position="-0.3 0.1 0"></a-entity>`
+//   // el.appendChild(child)
+//   // el.appendChild(text)
+// }
 
 AFRAME.registerComponent('cursor-listener', {
   init: function () {
     this.el.addEventListener('click', function (e) {
-
-      console.log(e.target.getAttribute('data-choice'));
-
-      console.log('I was clicked!');
+      console.log('I was clicked!', e.target.getAttribute('data-choice'));
     });
     this.el.addEventListener('mouseenter', function (e) {
       console.log('Mouse enter!', e.target);
--- game/index.js
+++ game/index.js
@@ -2,7 +2,7 @@ const questions = require('./questions');
 const yo = require('yo-yo');
 
 var Game = function() {
-	
+
 	this.step = 0;
 
 	this.init = () => {
@@ -16,9 +16,12 @@ var Game = function() {
 
 			let position = this.position();
 			let rotation = this.rotation(position);
-			let choice =  questions[this.step].choices[i];
+			let choice = questions[this.step].choices[i];
 
-	    	let entity = yo`<a-entity cursor-listener obj-model="obj: #fly-b-obj; mtl: #fly-b-mtl" data-choice="${choice}" position="${position.x} ${position.y} ${position.z}" scale="1 1 1" rotation="${rotation.x} ${rotation.y} ${rotation.z}"></a-entity>`;
+	    	let entity = yo`<a-entity cursor-listener obj-model="obj: #fly-b-obj; mtl: #fly-b-mtl" data-choice="${choice}" position="${position.x} ${position.y} ${position.z}" scale="1 1 1" rotation="${rotation.x} ${rotation.y} ${rotation.z}">
+						<a-animation attribute="scale" dur="200" fill="forwards" to="1 1 1" repeat="0"></a-animation>
+						<a-entity material="color: white" text="text: ${choice}; size: 0.24" position="-0.2 0.1 0"></a-entity>
+				</a-entity>`;
 
 			scene.appendChild(entity)
 			console.log('Added Question');
@@ -29,7 +32,7 @@ var Game = function() {
 	this.position = () => {
 
 		let position = {
-			x: this.getRand(-10, 10), 
+			x: this.getRand(-10, 10),
 			y: this.getRand(1, 3),
 			z: this.getRand(-2, -12),
 		}
@@ -40,9 +43,9 @@ var Game = function() {
 	this.rotation = (position) => {
 
 		let rotation = {
-			x: this.getRand(-5, 5), 
+			x: this.getRand(-5, 5),
 			y: position.x * -8,
-			z: this.getRand(-5, 5), 
+			z: this.getRand(-5, 5),
 		}
 
 		return rotation;
@@ -53,7 +56,7 @@ var Game = function() {
 	}
 }
 
-window.onload = function() { 
+window.onload = function() {
 
 	var game = new Game();
 	let scene = document.querySelector('#scene');
