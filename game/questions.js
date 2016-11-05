1415dd5d4c1d90974d6b82bde74cad2d754d9831
katiaeirin <katiaeirin@users.noreply.github.com>
2016-11-05 19:31:58 +1100

    add game & questions js

---
+++ game/questions.js
@@ -0,0 +1,52 @@
+module.exports = [
+			{
+				question: "5 x 5",
+				choices: [25, 76, 12, 20, 23],
+				answer: 0,
+			},
+			{
+				question: "5 x 2",
+				choices: [8, 10, 12, 20, 23],
+				answer: 1,
+			},
+			{
+				question: "12 x 2",
+				choices: [24, 22, 20, 30, 23],
+				answer: 0,
+			},
+			{
+				question: "6 x 8",
+				choices: [40, 32, 48, 46, 62],
+				answer: 2,
+			},
+			{
+				question: "7 x 4",
+				choices: [29, 16, 28, 46, 32],
+				answer: 2,
+			},
+			{
+				question: "5 x 5",
+				choices: [25, 76, 12, 24, 23],
+				answer: 0,
+			},
+			{
+				question: "5 x 2",
+				choices: [8, 10, 12, 20, 23],
+				answer: 1,
+			},
+			{
+				question: "12 x 2",
+				choices: [24, 22, 20, 30, 23],
+				answer: 0,
+			},
+			{
+				question: "6 x 8",
+				choices: [40, 32, 48, 46, 62],
+				answer: 2,
+			},
+			{
+				question: "7 x 4",
+				choices: [29, 16, 28, 46, 32],
+				answer: 2,
+			}
+		]
