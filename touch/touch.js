4663e5c5b493ff07671ea7b65ff3aab9d7916c38
doloreel <jarimado@gmail.com>
2016-11-05 13:51:04 +1100

    launching touch/gaze events indiscrimenatly

--- touch/touch.js
+++ touch/touch.js
@@ -1,2 +1,21 @@
 let hi= 'poo'
 console.log(hi)
+
+AFRAME.registerComponent('cursor-listener', {
+  init: function () {
+    console.log('cursor initialised')
+    var COLORS = ['red', 'green', 'blue'];
+    this.el.addEventListener('click', function () {
+      var randomIndex = Math.floor(Math.random() * COLORS.length);
+      this.setAttribute('material', 'color', COLORS[randomIndex]);
+      console.log('I was clicked!');
+    });
+    this.el.addEventListener('mouseenter', function () {
+      console.log('Mouse enter!');
+    });
+    this.el.addEventListener('mouseleave', function () {
+      console.log('Mouse leave!');
+    });
+
+  }
+});
