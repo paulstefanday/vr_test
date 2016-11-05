cd4eb01a5892728f050bd32064d9d9f4904a8fd5
Jeremy Arimado <jarimado@gmail.com>
2016-11-05 20:32:52 +1100

    looping cloud animation

--- game/index.js
+++ game/index.js
@@ -1,20 +1,15 @@
 const questions = require('./questions');
 
 var Game = function() {
-	
-	this.init = () => {
 
+	this.init = () => {
 		console.log(`Init`);
 		this.question(0);
 	}
 
 	this.question = (step) => {
-
-
 		console.log(`Question:`);
-
 		console.log(questions[step]);
-
 	}
 }
 
