6ab5b97cedc6140942316e13f614cf8f94a3855f
Paul <paul@paulday.com.au>
2016-11-06 14:20:33 +1100

    fixed bug

--- game/index.js
+++ game/index.js
@@ -5,7 +5,7 @@ var Game = function() {
 
 	this.step = 0;
 	this.score = 0;
-	this.duration = 3000;
+	this.duration = 30000;
 
 	this.init = () => {
 		setTimeout(() => {
@@ -67,14 +67,14 @@ var Game = function() {
 	// Sets Timer
 	this.timer = () => {
 		//3d9fba
-		let timerContainer = yo`<a-plane color="#ffffff" position="0 0.85 -1.5" height="0.05" width="0.8"></a-plane>`;
+		let timerContainer = yo`<a-plane id="timercontainer" color="#ffffff" position="0 0.85 -1.5" height="0.05" width="0.8"></a-plane>`;
 		let timer = yo`<a-plane timer color="#ef5a30" data-duration="${this.duration}" position="0 0 0" height="0.05" width="0.8"></a-plane>`;
 		let camera = document.querySelector('#camera');
 
 		timerContainer.appendChild(timer)
 		camera.appendChild(timerContainer);
 
-		setTimeout(this.result, 0);
+		setTimeout(this.result, this.duration);
 	}
 
 	this.result = () => {
@@ -82,10 +82,11 @@ var Game = function() {
 		$('#question').attr('visible', 'false')
 		$('#question-text').attr('visible', 'false')
 		$('#box').attr('visible', 'false')
+		$('#timercontainer').attr('visible', 'false')
 
 		// show sign
-		let show = yo`<a-animation attribute="position" dur="800" fill="forwards" to="1 -0.25 -2.77" repeat="0"></a-animation>`
-		$('#endfish').appendChild(show)
+		// let show = yo`<a-animation attribute="position" dur="800" fill="forwards" to="1 -0.25 -2.77" repeat="0"></a-animation>`
+		// $('#endfish').appendChild(show)
 
 		console.log("End of the game!");
 	}
