aa627c0c535634bec1092ef5deb4bf482775ad8d
Jeremy Arimado <jarimado@gmail.com>
2016-11-06 12:46:37 +1100

    progress on success

--- game/events.js
+++ game/events.js
@@ -28,6 +28,19 @@ function leaveAnimation(el) {
 	//el.appendChild(text)
 }
 
+function answerResponse(el) {
+	console.log(el)
+	// el.innerHTML = ''
+	debugger;
+	let parent = el.parentElement;
+	let choice = el.getAttribute('data-choice');
+	let child = yo`<a-animation attribute="scale" dur="200" fill="forwards" to="1.5 1.5 1.5" repeat="0"></a-animation>`;
+	//let text = yo`<a-entity material="color: white" text="text: ${choice}; size: 0.3" position="-0.3 0.1 0"></a-entity>`
+	parent.appendChild(child)
+	//el.appendChild(text)
+}
+
+
 AFRAME.registerComponent('cursor-listener', {
 	init: function () {
 		// this.el.addEventListener('click', function (e) {
@@ -36,6 +49,8 @@ AFRAME.registerComponent('cursor-listener', {
 		this.el.addEventListener('mouseenter', function (e) {
 			console.log('Mouse enter!', e.target);
 				addAnimation(e.target)
+				const el_status = document.querySelectorAll('#question-response')
+				answerResponse(el_status)
 		});
 		this.el.addEventListener('mouseleave', function (e) {
 			console.log('Mouse leave!', e.target);
